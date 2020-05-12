53fd74fa2253a2b5d40d848c512f7017d63cb457
Maciej Uberna <maciej.uberna@gmail.com>
2020-05-12 18:46:59 +0200

    Lesson428 "More on useState() & State Updating" Lesson429 "Array Destructuring" upading state in the most correct way and in just correct way.

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+  //it's better to use strings because throm input we always get strings
+  //useState returns always an array with exackly 2 elements:
+  //1. current state snapshot, for this rerender cycle of this component
+  //2. function that allows you to update your current state
+  const [ing, setIng] = useState({tile: '', amount: ''});
+
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -15,11 +21,23 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            {/* This first imput updates the state in the most correct way ensuring that all elements of state will be updated to the latest one.*/}
+            <input type="text" id="title" value={ing.title} onChange={event => {
+              const newTitle = event.target.value;
+              setIng(prevState => ({
+                ...prevState,
+                title: newTitle
+                })
+              )
+              }
+            }/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount" value={ing.amount} onChange={event => setIng({
+              ...ing,
+              amount: event.target.value,
+            })}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
