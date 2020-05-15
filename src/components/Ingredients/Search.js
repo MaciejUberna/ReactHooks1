6c90f815e178a153830c0e3a75a04405a5082b22
Maciej Uberna <maciej.uberna@gmail.com>
2020-05-15 15:26:54 +0200

    Lesson438 "Working with Refs & usieRef()" have set up a bunch of timers that run everytime I hit key on keyboard. It's not the way I want.

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,31 +6,42 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [filter, setFilter] = useState('');
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query = filter.length === 0 
-    ? '' 
-    : `?orderBy="title"&equalTo="${filter}"`;
-    fetch('https://maciej-hooks-update.firebaseio.com/ingredients.json'+query)
-    .then(response => response.json())
-    .then(responseData => {
-      const loadedIngredients= [];
-      for(const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          ...responseData[key]
+    //Here we compare value filter from past 1/2s and current filter value
+    setTimeout(() => {
+      if(filter === inputRef.current.value) {
+        const query = filter.length === 0 
+          ? '' 
+          : `?orderBy="title"&equalTo="${filter}"`;
+        fetch('https://maciej-hooks-update.firebaseio.com/ingredients.json'+query)
+        .then(response => response.json())
+        .then(responseData => {
+          const loadedIngredients= [];
+          for(const key in responseData) {
+            loadedIngredients.push({
+              id: key,
+              ...responseData[key]
+            });
+          };
+          onLoadIngredients(loadedIngredients)
         });
-      };
-      onLoadIngredients(loadedIngredients)
-    });
-  },[filter, onLoadIngredients])
+      }
+    },500);
+  },[filter, onLoadIngredients, inputRef])
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={filter} onChange={e => setFilter(e.target.value)}/>
+          <input 
+            ref={inputRef}
+            type="text" 
+            value={filter} 
+            onChange={e => setFilter(e.target.value)}
+          />
         </div>
       </Card>
     </section>
