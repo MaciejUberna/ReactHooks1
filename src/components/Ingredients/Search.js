78f61fc41d1a3479fb9a274e21977eb77de61dfe
Maciej Uberna <maciej.uberna@gmail.com>
2020-05-18 20:05:22 +0200

    Lesson449 "Using the Custom Hook" Added custom hook to the Search.js component.

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -4,11 +4,14 @@ import Card from '../UI/Card';
 import ErrorModal from '../UI/ErrorModal';
 import './Search.css';
 
+import useHttp from '../../hooks/http';
+
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [filter, setFilter] = useState('');
   const inputRef = useRef();
-  const [ error, setError ] = useState();
+  //const [ error, setError ] = useState();
+  const {isLoading, data, error, sendRequest, clear} = useHttp();
 
   useEffect(() => {
     //Here we compare value filter from past 1/2s and current filter value
@@ -17,20 +20,7 @@ const Search = React.memo(props => {
         const query = filter.length === 0 
           ? '' 
           : `?orderBy="title"&equalTo="${filter}"`;
-        fetch('https://maciej-hooks-update.firebaseio.com/ingredients.json'+query)
-        .then(response => response.json())
-        .then(responseData => {
-          const loadedIngredients= [];
-          for(const key in responseData) {
-            loadedIngredients.push({
-              id: key,
-              ...responseData[key]
-            });
-          };
-          onLoadIngredients(loadedIngredients)
-        }).catch(error => {
-          setError(error.message)
-        });
+        sendRequest('https://maciej-hooks-update.firebaseio.com/ingredients.json'+query,'GET');
       }
     },500);
     //The cleanup will run for previous effect before new effect is applied.
@@ -38,18 +28,28 @@ const Search = React.memo(props => {
     return () => {
       clearTimeout(timer);
     };
-  },[filter, onLoadIngredients, inputRef])
+  },[filter, sendRequest, inputRef])
 
-  const clearError = () => {
-    setError(null);
-  }
+  useEffect(() => {
+    if(!isLoading && !error && data) {
+      const loadedIngredients= [];
+      for(const key in data) {
+        loadedIngredients.push({
+          id: key,
+          ...data[key]
+        });
+      };
+      onLoadIngredients(loadedIngredients)
+    }
+  },[data, isLoading, error, onLoadIngredients]);
 
   return (
     <section className="search">
-      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal> }
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal> }
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
+          {isLoading && <span>Loading...</span>}
           <input 
             ref={inputRef}
             type="text" 
